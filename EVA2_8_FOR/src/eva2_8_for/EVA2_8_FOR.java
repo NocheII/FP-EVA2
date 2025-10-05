56a0aaa636b7ac0171b6d0f3a5df3dab42b3b452
NocheII <L25550865@chihuahua2.tecnm.mx>
2025-10-05 03:47:06 -0600

    Add files via upload

---
+++ EVA2_8_FOR/src/eva2_8_for/EVA2_8_FOR.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_for;
+/**
+ *
+ * @author Moren
+ */
+public class EVA2_8_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 1; i <= 10; i++ ) {
+            System.out.println(i);
+        }
+        
+        for (i = 10; i >= 1; i-- ) {
+            System.out.println(i);
+        }
+    }
+}
+
+        
+    
+  
