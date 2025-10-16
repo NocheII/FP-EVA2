664bc6976455bac0187d902a055c2acf2645c097
NocheII <L25550865@chihuahua2.tecnm.mx>
2025-10-15 21:47:08 -0600

    Add files via upload

---
+++ EVA2_15_ACUMULADORES/src/eva2_15_acumuladores/EVA2_15_ACUMULADORES.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_15_acumuladores;
+
+/**
+ *
+ * @author Moren
+ */
+public class EVA2_15_ACUMULADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int cuenta = 0;
+        cuenta = cuenta + 100;
+        System.out.println(cuenta);
+        cuenta = cuenta + 1000;
+        System.out.println(cuenta);
+        cuenta = cuenta - 800;
+        System.out.println(cuenta);
+        cuenta = cuenta + 2000;
+    
+        // TODO code application logic here
+    }
+    
+}
