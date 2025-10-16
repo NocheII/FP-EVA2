eef641826d514f54db09973da340b4d83fd31571
NocheII <L25550865@chihuahua2.tecnm.mx>
2025-10-15 23:23:11 -0600

    Add files via upload

---
+++ EVA2_16_ARREGLOS_MULDIMENSIONALES/src/eva2_16_arreglos_muldimensionales/EVA2_16_ARREGLOS_MULDIMENSIONALES.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_16_arreglos_muldimensionales;
+
+/**
+ *
+ * @author Moren
+ */
+public class EVA2_16_ARREGLOS_MULDIMENSIONALES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
